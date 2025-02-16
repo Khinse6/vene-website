071a29d7751f95c0e6ce60533b53b94312f03162
Khinse6 <henri23.2003@gmail.com>
2025-02-16 19:42:39 +0000

    Fixed NuxtHub (hopefully)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
-	ssr: true,
-	pages: true,
-	compatibilityDate: '2024-12-20',
-	modules: [
-		'@nuxtjs/seo',
-		'@nuxt/ui',
-		'@nuxt/fonts',
-		'@nuxt/image',
-		'@nuxt/eslint',
-		'@nuxtjs/supabase',
-		'@nuxthub/core',
-	],
-	devtools: {
-		enabled: true,
-		timeline: {
-			enabled: true,
-		},
-	},
-	css: ['~/assets/css/main.css'],
-	supabase: { redirect: false },
-	fonts: {
-		experimental: {
-			processCSSVariables: true,
-		},
-	},
-	vite: {
-		plugins: [tailwindcss()],
-		server: {
-			allowedHosts: true,
-		},
-	},
-	imports: { dirs: ['types'] },
-	nitro: { imports: { dirs: ['types'] }, experimental: { openAPI: true } },
+								ssr: true,
+								compatibilityDate: '2024-12-20',
+								modules: [
+								 '@nuxtjs/seo',
+								 '@nuxt/ui',
+								 '@nuxt/fonts',
+								 '@nuxt/image',
+								 '@nuxt/eslint',
+								 '@nuxtjs/supabase',
+								 '@nuxthub/core',
+								],
+								devtools: {
+																enabled: true,
+																timeline: {
+																								enabled: true,
+																},
+								},
+								css: ['~/assets/css/main.css'],
+								supabase: { redirect: false },
+								fonts: {
+																experimental: {
+																								processCSSVariables: true,
+																},
+								},
+								vite: {
+																plugins: [tailwindcss()],
+																server: {
+																								allowedHosts: true,
+																},
+								},
 })
