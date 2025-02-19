85dcba601dafe5b506324b7cc40d09006fcc26cb
Khinse6 <henri23.2003@gmail.com>
2025-02-19 14:28:12 +0000

    Update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -7,7 +7,7 @@
 				class="hover:text-orange-peel-500 flex items-center gap-5"
 				to="/"
 			>
-				<NuxtImg src="/logo.png" alt="Org Logo" sizes="40px" />
+				<NuxtImg src="/logo.png" alt="Org Logo" height="60" />
 				<div class="text-lg/5">
 					<p class="uppercase">Vengeance</p>
 					<p class="uppercase">Experts</p>
