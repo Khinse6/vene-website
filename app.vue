b267b6e381b2bdca7daaae1735f5d7276be2422d
Khinse6 <henri23.2003@gmail.com>
2025-02-20 14:52:31 +0000

    Update app.vue

--- app.vue
+++ app.vue
@@ -9,7 +9,9 @@
 <script setup lang="ts">
 	useSeoMeta({
 		ogImage: 'https://vengeance-experts.nuxt.dev/og-image.png',
+		twitterTitle: 'Vengeance Experts',
 		twitterCard: 'summary_large_image',
+		twitterImage: 'https://vengeance-experts.nuxt.dev/og-image.png',
 		ogLocale: 'pt',
 	})
 
